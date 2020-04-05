ebbfe5205b52d5920f75c3ff0a00135e6d559255
Tpoppp <olson0002@yahoo.com>
2020-04-04 18:42:06 -0700

    added local storage and some comments

--- develop/script.js
+++ develop/script.js
@@ -1,26 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // relative time 
- let todayEl = $("#currentDay");
- todayEl.text(moment().format("MMMM Do YYYY, h:mm:ss a"))
+  let today = $("#currentDay");
+  today.text(moment().format("MMMM Do YYYY, h:mm:ss a"))
 
-    //   const timEl = $("<")
-    // moment().format('MMMM Do YYYY, h:mm:ss a');
+  let currentTime = moment();
+  
+  // timeblocks 
+  let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  
+  let userInput = $("#formGroupExampleInput").val();
+  // for loop 
+  for (i = 0; i < hours.length; i++) {
 
-  // timeblocks elements
-   // let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-   // hours = $("<div>");
+    let blocktime = $(".row" + hours[i]);
 
-   // hours.addClass("time")
-   // $(".container").append(hours);
-   
-  //   let todayDate = $("#currentDay");
-  // todayDate.text(now);
+    if (currentTime == hours[i]) {
+      console.log(i);
+      $(".row" + hours[i]).addClass("greybackground");
+    } else if (currentTime > hours[i]) {
+      $(".row" + hours[i]).addClass("redbackground");
+    } else if (currentTime < hours[i]) {
+      $(".row" + hours[i]).addClass("greenbackground");
+    }
+  }
+  // append to timeblock
+
+   $(".formGroupExampleInput").append(userInput);
+
+
 
   // save button for timeblocks
-  // $(".saveBtn").on("click", function(){
-  //   let userInput = $("<div>");
-  //   userInput = $(this).val();
-  //   $(".container").val();
-  //   $(".container").append(userInput);
-  // })
+  $(".saveBtn").on("click", function () {
+    event.preventDefault();
+    let saveBtn = "savebtn";
+    let all = {
+      saveBtn: saveBtn,
+      currentTime: currentTime,
+    }
+    //local storage
+  let userInput = localStorage.setItem("#formGroupExampleInput", userInput);
+
+    
+    // $("#formGroupExampleInput").val();
+    // $("#formGroupExampleInput").append(userInput);
+  });
 });
